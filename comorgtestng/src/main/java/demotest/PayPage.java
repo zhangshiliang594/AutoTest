69dc69c0427f0f353c86acf10a6b573f065b3e7a
wanglin <wanglin>
2021-07-30 14:33:33 +0800

    新增代码

---
+++ comorgtestng/src/main/java/demotest/PayPage.java
@@ -0,0 +1,14 @@
+package demotest;
+
+/**
+ * FileName: PayPage
+ * Author:   wanglin
+ * Date:     2021/7/30 14:01
+ * Description: ${DESCRIPTION}
+ * version: IT2021${Iteration}
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class PayPage {
+}
